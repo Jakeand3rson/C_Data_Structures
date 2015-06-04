b7ddd763193966cd418f26cdffa414374b2dda1b
Jakealope <jake.anderson486@gmail.com>
2015-06-04 16:09:21 -0700

    added pointer to .h file

--- merge_sort.c
+++ merge_sort.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "merge_sort.h"
 // This program will impliment merge sort
+
+void merge(int *arr, int left_start, int right_start, int right_end) {
+
+}
 
