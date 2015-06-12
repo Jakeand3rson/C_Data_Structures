7df57e6b774f0915f1ab9bd5d21fff94a1f6d9ce
Jakealope <jake.anderson486@gmail.com>
2015-06-12 13:45:39 -0700

    added nested for loops.

--- selection_sort.c
+++ selection_sort.c
@@ -6,4 +6,10 @@ void select_sort (int s[], int n) {
     int i,j;
     int min;
 
-    
+    for (i=0; i<n; i++) {
+        min = i;
+        for (j = i+1; j < n; j++) {
+            if (s[j] < s[min]) min = j;
+        swap(&s[i], &s[min]);
+        }
+    }
