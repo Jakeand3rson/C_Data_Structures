a18b27e9a799447f7dc535c580eca8b176fb8349
Jakealope <jake.anderson486@gmail.com>
2015-06-12 14:18:30 -0700

    added function to replace swap. Turns out thats not a thing. Also changed a cariable name.

--- selection_sort.c
+++ selection_sort.c
@@ -2,30 +2,35 @@
 #include <stdlib.h>
 // This file contains the algorithm for Selection Sort
 // pg 42 The Algorithm Design Manual Second Edition
-void select_sort (int s[], int n) {
-    int i,j;
+void select_sort (int *arr, int n) {
+    int i, j, temp;
     int min;
 
     for (i=0; i<n; i++) {
         min = i;
         for (j = i+1; j < n; j++) {
-            if (s[j] < s[min]) min = j;
-        swap(&s[i], &s[min]);
+            if (arr[j] < arr[min]) 
+                min = j;
+        }
+        if (min != i) {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
-
-in main() {
-    int a[8] = {8,7,6,5,4,3,2,1,0};
-    printf("The unsorted array is: \n");
+}
+int main() {
+    int a[8] = {,7,6,5,4,3,2,1};
+    printf("the unsorted array is: \n");
     int i = 0;
-    for (i= 0; i< 8; i++) {
+    for(i = 0;i<8;i++){
         printf("\t%d", a[i]);
     }
     printf("\n");
 
     select_sort(a, 8);
-    printf("The sorted array is :\n");
-    for(i = 0, i < 8; i++) {
+    printf("the sorted array is: \n");
+    for(i=0;i<8;i++){
         printf("\t%d", a[i]);
     }
     printf("\n");
