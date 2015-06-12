682b2c90e863d9a8be7753d23b1a6f1e2157a059
Jakealope <jake.anderson486@gmail.com>
2015-06-12 14:00:36 -0700

    added main funtion for testing

--- selection_sort.c
+++ selection_sort.c
@@ -13,3 +13,20 @@ void select_sort (int s[], int n) {
         swap(&s[i], &s[min]);
         }
     }
+
+in main() {
+    int a[8] = {8,7,6,5,4,3,2,1,0};
+    printf("The unsorted array is: \n");
+    int i = 0;
+    for (i= 0; i< 8; i++) {
+        printf("\t%d", a[i]);
+    }
+    printf("\n");
+
+    select_sort(a, 8);
+    printf("The sorted array is :\n");
+    for(i = 0, i < 8; i++) {
+        printf("\t%d", a[i]);
+    }
+    printf("\n");
+}
