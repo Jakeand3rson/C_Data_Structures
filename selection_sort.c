079ccec617acf46e37f2b02ff5004cced758cfdf
Jakealope <jake.anderson486@gmail.com>
2015-06-12 13:45:03 -0700

    added function for selection sort with some variables

---
+++ selection_sort.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+// This file contains the algorithm for Selection Sort
+// pg 42 The Algorithm Design Manual Second Edition
+void select_sort (int s[], int n) {
+    int i,j;
+    int min;
+
+    
