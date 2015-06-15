ad38a048f04325b0c49ad7692b0345dcdb3f809c
Jakealope <jake.anderson486@gmail.com>
2015-06-15 16:35:43 -0700

    added memory allocation check

--- merge_sort.c
+++ merge_sort.c
@@ -3,7 +3,38 @@
 #include "merge_sort.h"
 // This program will impliment merge sort
 
+// This funtion will merge two sorted arrays
 void merge(int *arr, int l_start, int r_start, int r_end) {
+    int total = r_end - l_start;
+
+    int *temp = NULL;
+    temp = (int *) malloc(sizeof(int) * total);
+    if (temp == NULL)
+        fprintf(stderr, "Failed memory allocation \n");
+
+    int i = l_start;
+    int j = r_start;
+    int k = 0;
+
+    while (i < r_start && j < r_end) {
+        if(arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i < r_start) {
+        temp[k++] = arr[i++];
+    }
+
+    while (i < r_end) {
+        temp[k++] = arr[j++];
+    }
+
+    for (i = 0; j = l_start; i < total; i++; j++)
+        arr[j] = temp[i];
+
+    free(temp);
 
 }
 
