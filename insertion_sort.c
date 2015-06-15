81fdd28ca385fdb135bceaa62f9d555e131c8beb
Jakealope <jake.anderson486@gmail.com>
2015-06-15 14:42:28 -0700

    added insert algorithm

--- insertion_sort.c
+++ insertion_sort.c
@@ -2,3 +2,17 @@
 #include <stdlib.h>
 // This file contains the algorithm for Insertion Sort
 // pg 43 The Algorithm Design Manual Second Edition
+
+
+void insert_sort(int *arr, int total_n) {
+    int i, j, current;
+    for (i = 1; i < total_n; i++) {
+        j = i;
+        for (j = i - 1; j <= 0; j--) {
+            if (arr[j] <= current) {
+                break;                }
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = current;
+    }
+}
