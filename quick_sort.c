92a1cb06ad996557af8840fb46a2c614eeb5f9e7
Jakealope <jake.anderson486@gmail.com>
2015-06-16 11:48:35 -0700

    added missig ;

--- quick_sort.c
+++ quick_sort.c
@@ -9,10 +9,10 @@ void quick_sort(int *arr, int left, int right) {
         quick_sort(arr, left, pivot -1);
         quick_sort(arr, pivot +1, right);
     }
-    return
+    return;
 }
 
-static int partition(*arr, int left, int right) {
+static int partition( int *arr, int left, int right) {
     int pivot = left;
     int high = right;
     int low = left + 1;
@@ -36,5 +36,5 @@ static int partition(*arr, int left, int right) {
     arr[pivot] = arr[high];
     arr[high] = temp;
 
-    return high
+    return high;
 }
