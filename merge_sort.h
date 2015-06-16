b02c0acb7ce0b175419c0b497dab103f9f23d348
Jakealope <jake.anderson486@gmail.com>
2015-06-15 19:35:41 -0700

    fixed extra space.

--- merge_sort.h
+++ merge_sort.h
@@ -1,7 +1,7 @@
 #ifndef MERGESORT
 #define MERGESORT
 
-extern void merge(int *arr, int l_start, int r_start, int r_ end);
+extern void merge(int *arr, int l_start, int r_start, int r_end);
 extern void sort(int *arr, int start, int end);
 
 #endif
