2b8bdad8c344d9688e13368ccd5edeab349c8669
Jakealope <jake.anderson486@gmail.com>
2015-06-15 18:58:03 -0700

    added main function. There is a memory allocation error. broken

--- merge_sort.c
+++ merge_sort.c
@@ -4,7 +4,7 @@
 // This program will impliment merge sort
 
 // This funtion will merge two sorted arrays
-void merge(int *arr, int l_start, int r_start, int r_end) {
+void merge (int *arr, int l_start, int r_start, int r_end) {
     int total = r_end - l_start;
 
     int *temp = NULL;
@@ -31,7 +31,7 @@ void merge(int *arr, int l_start, int r_start, int r_end) {
         temp[k++] = arr[j++];
     }
 
-    for (i = 0; j = l_start; i < total; i++; j++)
+    for (i = 0, j = l_start; i < total; i++, j++)
         arr[j] = temp[i];
 
     free(temp);
@@ -39,7 +39,7 @@ void merge(int *arr, int l_start, int r_start, int r_end) {
 }
 
 //recursively sorting
-void sort(int *arr, int start, int end){
+void sort (int *arr, int start, int end) {
     int total = end - start;
     int l_start = start;
     int r_start = (total % 2 == 0) ? (start + end) / 2 : (start + end + 1) / 2;
@@ -49,9 +49,36 @@ void sort(int *arr, int start, int end){
     if (total < 2)
         return;
 
-    // if its just two items.. Just call merge()
-    if (total == 2)
+    // If its just two items.. Just call merge()
+    else if (total == 2)
         merge(arr, l_start, r_start, r_end);
 
-    
+    // If the array is bigger than two items, recursively call merge()
+
+    else {
+        if (r_start - l_start > 1)
+            sort(arr, l_start, r_start);
+        if (r_end - r_start > 1)
+            sort(arr, r_start, r_end);
+
+
+        // After the recursion is finished, merge that shit.
+        merge(arr, l_start, r_start, r_end);
+    }
+
+}
+
+int main() {
+    int a[8] = {8,7,9,5,4,3,2,1};
+    printf("The unsorted array is: \n");
+    int i = 0;
+    for (i = 0; i < 8; i++)
+        printf("\t%d", a[i]);
+    printf("\n");
+    sort(a, 0, 8);
+    printf("the sorted array is: \n");
+    for(i = 0; i < 8; i++){
+        printf("\t%d", a[i]);
+    }
+    printf("\n");
 }
