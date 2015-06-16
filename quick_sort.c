8b82a30dc7d040c9c5b2562991ae1e3e7614cd44
Jakealope <jake.anderson486@gmail.com>
2015-06-16 11:21:48 -0700

    fleshed out function, recursion was used.....

--- quick_sort.c
+++ quick_sort.c
@@ -1,5 +1,13 @@
 #include "quick.h"
 
 static int quick_sort(int *arr, int left, int right) {
-    break
+    int pivot;
+
+    if (left <= right) {
+        pivot = partition(arr, left, right);
+
+        quick_sort(arr, left, pivot -1);
+        quick_sort(arr, pivot +1, right);
+    }
+    return
 }
