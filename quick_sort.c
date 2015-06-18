449f72d8820324957b3b6d6d1b391cd5df61dec6
Jakealope <jake.anderson486@gmail.com>
2015-06-18 14:09:48 -0700

    commented out old algorithm. Starting fresh with a simpler algorithm.

--- quick_sort.c
+++ quick_sort.c
@@ -3,46 +3,51 @@
 #include "quick_sort.h"
 
 
+void quick_sort(int *arr, int n) {
+    int left, right, pivot, temp;
 
-static int partition( int *arr, int left, int right) {
-    int pivot = left;
-    int high = right;
-    int low = left + 1;
-    int temp;
-
-    while (low <= high) {
-        while (arr[pivot] >= arr[low]) {
-            low++;
-        }
-        while (arr[pivot] <= arr[high]) {
-            high--;
-        }
-        if (low <= high) {
-            temp = arr[high];
-            arr[high] = arr[low];
-            arr[low] = temp;
-        }
-    }
-
-    temp = arr[pivot];
-    arr[pivot] = arr[high];
-    arr[high] = temp;
-
-    return high;
 }
 
-void quick_sort(int *arr, int left, int right) {
-    int pivot;
 
-    // divide and conquer
-    if (left <= right) {
-        pivot = partition(arr, left, right);
+// static int partition( int *arr, int left, int right) {
+//     int pivot = left;
+//     int high = right;
+//     int low = left + 1;
+//     int temp;
 
-        quick_sort(arr, left, pivot - 1);
-        quick_sort(arr, pivot +1, right);
-    }
-    return;
-}
+//     while (low <= high) {
+//         while (arr[pivot] >= arr[low]) {
+//             low++;
+//         }
+//         while (arr[pivot] <= arr[high]) {
+//             high--;
+//         }
+//         if (low <= high) {
+//             temp = arr[high];
+//             arr[high] = arr[low];
+//             arr[low] = temp;
+//         }
+//     }
+
+//     temp = arr[pivot];
+//     arr[pivot] = arr[high];
+//     arr[high] = temp;
+
+//     return high;
+// }
+
+// void quick_sort(int *arr, int left, int right) {
+//     int pivot;
+
+//     // divide and conquer
+//     if (left <= right) {
+//         pivot = partition(arr, left, right);
+
+//         quick_sort(arr, left, pivot - 1);
+//         quick_sort(arr, pivot +1, right);
+//     }
+//     return;
+// }
 
 int main()
 {
