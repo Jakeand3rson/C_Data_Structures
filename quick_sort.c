989f14a9df2af689f776e0a604d0d1a312a7cf1c
Jakealope <jake.anderson486@gmail.com>
2015-06-18 14:48:26 -0700

    removed commented out stuff.

--- quick_sort.c
+++ quick_sort.c
@@ -30,46 +30,6 @@ void quick_sort(int *arr, int n) {
 }
 
 
-// static int partition( int *arr, int left, int right) {
-//     int pivot = left;
-//     int high = right;
-//     int low = left + 1;
-//     int temp;
-
-//     while (low <= high) {
-//         while (arr[pivot] >= arr[low]) {
-//             low++;
-//         }
-//         while (arr[pivot] <= arr[high]) {
-//             high--;
-//         }
-//         if (low <= high) {
-//             temp = arr[high];
-//             arr[high] = arr[low];
-//             arr[low] = temp;
-//         }
-//     }
-
-//     temp = arr[pivot];
-//     arr[pivot] = arr[high];
-//     arr[high] = temp;
-
-//     return high;
-// }
-
-// void quick_sort(int *arr, int left, int right) {
-//     int pivot;
-
-//     // divide and conquer
-//     if (left <= right) {
-//         pivot = partition(arr, left, right);
-
-//         quick_sort(arr, left, pivot - 1);
-//         quick_sort(arr, pivot +1, right);
-//     }
-//     return;
-// }
-
 int main()
 {
     int arr[] = { 3, 5, 9, 10, 13, 100, 32, 41, -86, 1, 0, 2 };
