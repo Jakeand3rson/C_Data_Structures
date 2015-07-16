781ef62a7f6faa434bdeb4d6e526130bcf5f179b
Jakealope <jake.anderson486@gmail.com>
2015-07-16 13:48:01 -0700

    finished algorithm

--- bubble_sort.c
+++ bubble_sort.c
@@ -5,7 +5,12 @@ void bubble_sort (int *a, int n) {
     while (s) {
         s = 0;
         for (i = 1; i < n; i++) {
-           
+            if(a[i] < a[i - 1]) {
+                t = a[i];
+                a[i] = a[i - 1];
+                a[i - 1] = t;
+                s = 1;
+            }
         }
     }
 }
