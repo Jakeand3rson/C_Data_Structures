3fd54b622a0591abf6d277067d8f5a554215957a
Jakealope <jake.anderson486@gmail.com>
2015-07-16 13:47:10 -0700

    added beginning of algorithm

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 void bubble_sort (int *a, int n) {
-    
+    int i, t, s = 1;
+    while (s) {
+        s = 0;
+        for (i = 1; i < n; i++) {
+           
+        }
+    }
 }
