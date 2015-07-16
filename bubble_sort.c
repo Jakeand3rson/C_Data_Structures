fe8396262a587990f254658c77e8968862efc863
Jakealope <jake.anderson486@gmail.com>
2015-07-16 15:10:40 -0700

    added random generator to main function

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void bubble_sort (int *a, int n) {
     int i, t, s = 1;
@@ -15,19 +16,36 @@ void bubble_sort (int *a, int n) {
     }
 }
 
-int main() {
-    int a[] = {4, 65, 76, 34, 453, 23, 65, 76, 87, 67, 433};
+// int main() {
+//     // int a[] = {4, 65, 76, 34, 453, 23, 65, 76, 87, 67, 433};
+//     int i;
+//     int a[100];
+//     int n = rand() % 100 + 1;
+//     for (i = 0; i < n; i++) {
+//         printf("%d%s", a[i], i == n - 1 ? "\n" : " ");
+//     }
+//     bubble_sort(a, n);
+//     for (i = 0; i < n; i++) {
+//         printf("%d%s", a[i], i == n - 1 ? "\n" : " ");
+//     }
+//     return 0;
+
+// }
+int main () {
+    int a[20];
+    for(int i = 1; i < 20; ++i){
+      a[i] = rand() % 1000 + 1;
+    }
     int n = sizeof a / sizeof a[0];
     int i;
     printf("The pre-sorted numbers are...\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++){
         printf("%d%s", a[i], i == n - 1 ? "\n" : " ");
     }
     bubble_sort(a, n);
     printf("The post-sorted numbers are...\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++){
         printf("%d%s", a[i], i == n - 1 ? "\n" : " ");
     }
     return 0;
-
 }
